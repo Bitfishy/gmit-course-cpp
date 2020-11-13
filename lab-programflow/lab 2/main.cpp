b87263304a0ee640b5253c9b037173dbf4780614
Bitfishy <acdc.elec@gmail.com>
2020-11-13 13:39:51 +0000

    t2

---
+++ lab-programflow/lab 2/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+
+    void LabInfo(){
+    cout << "Stephen McDonagh\n";
+    cout << "Lab #2" << endl;
+    }
+
+    int main(){
+    LabInfo:
+
+    if (x > y);
+    {
+    cout << "The largest value is " << x;
+}
+    else{
+    cout << "The largest value is " << y;
+
+    return 0;
+
+}
